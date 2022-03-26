daae3f68e25516af52b12bf0d8ed9d0f654deb27
christine-zhe <christinez@ucla.edu>
2022-03-26 10:45:44 -0700

    edited API routes to display save button

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,14 +1,12 @@
 const router = require("express").Router();
 
-// const { append } = require("express/lib/response");
 const fs = require('fs');
 const path = require('path');
-// const { v4: uuidv4 } = require("uuid");
-let allNotes = require("../../db/db.json");
+const {allNotes} = require("../../db/db.json");
 
 router.get("/notes", (req, res) => {
-    
-  res.json(allNotes);
+  let results = allNotes;
+  res.json(results);
 });
 
 
